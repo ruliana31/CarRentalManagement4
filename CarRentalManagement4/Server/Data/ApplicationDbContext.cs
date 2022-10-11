99498c7dd2382deca7a13516360f925ddfbd7682
b46usf <b46usf@gmail.com>
2022-10-11 19:52:02 +0700

    Menambahkan table

--- CarRentalManagement4/Server/Data/ApplicationDbContext.cs
+++ CarRentalManagement4/Server/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using CarRentalManagement4.Server.Models;
+using CarRentalManagement4.Shared.Domain;
 using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
@@ -17,5 +18,11 @@ namespace CarRentalManagement4.Server.Data
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
     }
 }
--- CarRentalManagement4/Server/Data/Migrations/20221011124527_AddedApplicationTables.Designer.cs
+++ CarRentalManagement4/Server/Data/Migrations/20221011124527_AddedApplicationTables.Designer.cs
@@ -4,14 +4,16 @@ using CarRentalManagement4.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarRentalManagement4.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221011124527_AddedApplicationTables")]
+    partial class AddedApplicationTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
