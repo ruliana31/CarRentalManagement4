257db203833ab0ec887253b2eb62878f170e367e
b46usf <b46usf@gmail.com>
2022-10-11 18:43:47 +0700

    Menambahkan firstname dan lastname di profile

--- CarRentalManagement4/Server/Models/ApplicationUser.cs
+++ CarRentalManagement4/Server/Models/ApplicationUser.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CarRentalManagement4.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +12,7 @@ namespace CarRentalManagement4.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
